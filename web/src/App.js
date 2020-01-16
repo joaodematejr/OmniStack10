e84c36ddc064a228dd2cee926a05ea56a2f73836
joaodematejr <joaodematejr@gmail.com>
2020-01-16 10:00:09 -0300

    finalização do sistema web

--- web/src/App.js
+++ web/src/App.js
@@ -1,161 +1,44 @@
 import React, { useEffect, useState } from 'react';
+import api from './services/api';
+
+
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
+
 function App() {
-  const [github_name, setGithubname] = useState('');
-  const [techs, setTechs] = useState('');
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
+  const [devs, setDevs] = useState([]);
+
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-        setLatitude(latitude);
-        setLongitude(longitude)
-      },
-      (err) => {
-        console.log(err)
-      },
-      {
-        timeout: 30000,
-      }
-    )
+    async function loadDevs() {
+      const response = await api.get('devs');
+      setDevs(response.data)
+    }
+    loadDevs()
   }, []);
+
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data)
+    setDevs([...devs, response.data])
+  }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_name">Usuário do Github</label>
-            <input
-              name="github_name"
-              id="username_github"
-              required
-              value={github_name}
-              onChange={e => setGithubname(e.target.value)}
-            />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input
-              name="techs"
-              id="techs"
-              required
-              value={techs}
-              onChange={e => setTechs(e.target.value)}
-            />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input
-                type="number"
-                name="latitude"
-                id="latitude"
-                required
-                value={latitude}
-                onChange={e => setLatitude(e.target.value)}
-              />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input
-                type="number"
-                name="longitude"
-                id="longitude"
-                required
-                value={longitude}
-                onChange={e => setLongitude(e.target.value)}
-              />
-            </div>
-          </div>
-
-          <button type="submit">
-            Salvar
-          </button>
-
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/15802727?s=460&v=4" alt="Habal" />
-              <div className="user-info">
-                <strong>
-                  João Dematé
-                </strong>
-                <span>
-                  ReactJS, React-Native, NodeJS
-                  </span>
-              </div>
-            </header>
-            <p>
-              Build world, in code. Mostly @rust-lang
-            </p>
-            <a href="https://github.com/DCjanus">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/15802727?s=460&v=4" alt="Habal" />
-              <div className="user-info">
-                <strong>
-                  João Dematé
-                </strong>
-                <span>
-                  ReactJS, React-Native, NodeJS
-                  </span>
-              </div>
-            </header>
-            <p>
-              Build world, in code. Mostly @rust-lang
-            </p>
-            <a href="https://github.com/DCjanus">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/15802727?s=460&v=4" alt="Habal" />
-              <div className="user-info">
-                <strong>
-                  João Dematé
-                </strong>
-                <span>
-                  ReactJS, React-Native, NodeJS
-                  </span>
-              </div>
-            </header>
-            <p>
-              Build world, in code. Mostly @rust-lang
-            </p>
-            <a href="https://github.com/DCjanus">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/15802727?s=460&v=4" alt="Habal" />
-              <div className="user-info">
-                <strong>
-                  João Dematé
-                </strong>
-                <span>
-                  ReactJS, React-Native, NodeJS
-                  </span>
-              </div>
-            </header>
-            <p>
-              Build world, in code. Mostly @rust-lang
-            </p>
-            <a href="https://github.com/DCjanus">Acessar perfil no Github</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
